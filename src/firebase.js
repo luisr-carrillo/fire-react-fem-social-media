894faa1cc68a6d07ab74aaf04b4fdb85a13e4305
Luis R. Carrillo <luisr_carrillo@outlook.com>
2020-03-02 12:24:40 -0600

    Configuración de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: 'AIzaSyBfia4sh3vu7Kfb3eVc3eQTtLkfavA7iRY',
     authDomain: 'react-fem-firebase.firebaseapp.com',
     databaseURL: 'https://react-fem-firebase.firebaseio.com',
@@ -10,17 +11,14 @@ const firebaseConfig = {
     storageBucket: 'react-fem-firebase.appspot.com',
     messagingSenderId: '546666617826',
     appId: '1:546666617826:web:9bf00bf24cfebcd98637bb',
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 export const firestore = firebase.firestore();
 export const auth = firebase.auth();
+export const storage = firebase.storage();
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
-
-window.firebase = firebase;
 
 export const getUserDocument = async (uid) => {
     if (!uid) return null;
@@ -61,5 +59,7 @@ export const createUserProfileDocument = async (user, additionalData) => {
 
     return getUserDocument(user.uid);
 };
+
+window.firebase = firebase;
 
 export default firebase;
